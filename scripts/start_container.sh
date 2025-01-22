4fccec19ff00e504a1fe987689e17ffd429544f3
Vickyproject <vs72385@gmail.com>
2025-01-22 17:46:41 +0530

    new script

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
+set e
 
-# Define the image you want to pull (replace with your image name and tag)
-IMAGE_NAME="simple-python-flask:latest"
 
 # Pull the Docker image
-docker pull $IMAGE_NAME
-
-# Define the container name
-CONTAINER_NAME="flask-app"
+docker pull lionpull123/simple-python-flask
 
 # Run the Docker container with the specified name and port mapping
-docker run -d --name $CONTAINER_NAME -p 5000:5000 $IMAGE_NAME
+docker run -d -p 5000:5000
